4405d75419ef926bb71f516da626a19952b7593a
Melissa Wheatley <melissa.wheatley@healthstream.com>
2018-11-19 17:25:14 -0600

    Updates

---
+++ precache-manifest.53cb89a882a838cb06c80f5bc449a997.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5591eaa0d527a768f984",
+    "url": "/newsfeed/static/js/runtime~main.5591eaa0.js"
+  },
+  {
+    "revision": "1ec1359a34ad424d351e",
+    "url": "/newsfeed/static/js/main.1ec1359a.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/newsfeed/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "1ec1359a34ad424d351e",
+    "url": "/newsfeed/static/css/main.760aa3e5.chunk.css"
+  },
+  {
+    "revision": "f65531c8da48ede3bec27ae3c0162075",
+    "url": "/newsfeed/index.html"
+  }
+];
